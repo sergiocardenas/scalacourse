0f4981b9d71d448a6b0de953bc980597b04c152a
Synergy-GB <Synergy-GB@Synergy-GB-PC>
2016-03-03 12:03:18 -0430

    Taller sergio comitenado super tarde

--- src/main/scala/org/scalacourse/Boot.scala
+++ src/main/scala/org/scalacourse/Boot.scala
@@ -2,11 +2,36 @@ package org.scalacourse
 
 import com.synergygb.zordon.core.ServiceBoot
 import com.synergygb.zordon.gen.routes.ApplicationRoutesConsolidated
+import com.synergygb.zordon.gen.models.ProjectInfo
+import spray.http.StatusCodes._
 
 object Boot extends App with ServiceBoot with ApplicationRoutesConsolidated {
 
-  implicit val context = Context
-
+  import Context.keyValueStore
+  import Context.executionContext
+  
+  implicit def context = Context
+  
   protected def apiResourceClass = getClass
+  
+  def dataContext = Context
+  
+  override def handlePutCreaProyecto(project: ProjectInfo)() = {
+    val key = project.nombreProyecto
+    onSuccess(keyValueStore.write(key, project)) { x =>
+      complete(Created, "Creacion exitosa del projecto")
+    }
+  }
 
+  override def handlePostEditarProyecto(project: ProjectInfo)() = {
+    val key = project.nombreProyecto
+    onSuccess(keyValueStore.read[ProjectInfo](key)) { maybeProj  =>
+      val proj = maybeProj.getOrElse(ProjectInfo(nombreProyecto="error",liderProyecto=None,fechaInicioProyecto=None,fechaFinProyecto=None,tareas=None))
+      if(proj.nombreProyecto == key){
+        onSuccess(keyValueStore.write(key, project)) { x =>
+          complete(Created, "Actualizaciom exitosa del projecto")
+        }
+      }else complete(NotFound)
+    }
+  }
 }
